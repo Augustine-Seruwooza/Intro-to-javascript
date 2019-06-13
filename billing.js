94fe54d75b5796d40b645d6341d4294f3d96cc73
Augustine-Seruwooza <aseruwooza@gmail.com>
2019-06-13 09:52:13 +0300

    my first js

---
+++ billing.js
@@ -0,0 +1,5 @@
+//This is the percentage thet determines the transaction charge//
+const amount=0.025;
+var amountTendered=100000;
+console.log('This is what the billed amount is(less transaction charges)');
+console.log(amountTendered-(amountTendered*amount));
