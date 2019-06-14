2ced7efd1d109bc74dff84751e20144290b602fd
Seruwooza Augustine <51474766+Augustine-Seruwooza@users.noreply.github.com>
2019-06-14 11:01:13 +0300

    js null checker

---
+++ null_checker.js
@@ -0,0 +1,17 @@
+var i;
+for(i=1; i<=10; i++)
+{
+	let number = prompt('Please input your number?','');
+	if(number > 0)
+	{
+		console.log(1);
+	}
+	else if(number < 0)
+	{
+		console.log(-1);
+	}
+	else
+	{
+		console.log(0);
+	}
+}
